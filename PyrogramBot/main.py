74f9c0412ce2b75d597589035544d0900a8af6bc
LucasHood-099 <101005185+LucasHood-099@users.noreply.github.com>
2022-05-03 11:56:16 +0530

    Create main.py

---
+++ PyrogramBot/main.py
@@ -0,0 +1,10 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config API_ID, API_HASH, BOT_TOKEN
+import random
+
+Nandhu=Client(
+    "Pyrogram Bot",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
