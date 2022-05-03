c464fdeb8fb294b3913a6c3260d780bf36d7e91f
LucasHood-099 <101005185+LucasHood-099@users.noreply.github.com>
2022-05-03 12:00:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+API_HASH= os.environ.get("4d68170d17348aba5c62656d6e879c1d")
+API_ID = int(os.environ.get("10631386"))
+BOT TOKEN= os.environ.get("5388097061:AAF9h8ODteA9-2SygPJBsLmqMRLp_gres9c")
